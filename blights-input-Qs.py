18bbad54881db4a151a9dd77c5ec4d4cb5e8955e
bururik <spacepopeleo@gmail.com>
2017-01-09 19:00:46 -0600

    cleaned up mini python projects

--- blights-input-Qs.py
+++ blights-input-Qs.py
@@ -2,9 +2,9 @@ blight = int(input('please input the number of blights in your area:'))
 
 ans = input('are you sure there are %d blight(s) in your area?' %(blight))
 if ans =='y':
-    print('Damn nigguh')
+    print('Damn')
 elif ans == "n":
     print('bitch you stupid')
     
-#obviously improvements can be made but i'm short on time and this was just a test pf the input function    
+#obviously improvements can be made but i'm short on time and this was just a test of the input function    
       
